5ee508479035c6afc3436b5cd530fecdfa832874
renanguilhermef <renanguilhermef@gmail.com>
2023-03-06 22:11:18 -0300

    added dependencias

--- jenkins.tf
+++ jenkins.tf
@@ -8,4 +8,30 @@ module "jenkins_sg" {
   ingress_cidr_blocks      = ["0.0.0.0/0"]
   ingress_rules            = ["http-80-tcp"]
   egress_rules             = ["all-all"]
+
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "jenkins-server"
+  ami                    = "ami-006dcf34c09e50022"
+  instance_type          = "t3.micro"
+  key_name               = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.jenkins_sg.security_group_id]
+  subnet_id              = "subnet-00cdde63de5502849"
+  user_data              =  file("./dependencias.sh")
+  iam_instance_profile   = "LabInstanceProfile"
+
+  tags = {
+    Name   = "Jenkins-Server"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_eip" "jenkins-ip" {
+  instance = module.ec2_instance.id
+  vpc      = true
 }
